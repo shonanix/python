937179f8907868537c0d335e5d7e559180ba69cf
shonanix <88073822+shonanix@users.noreply.github.com>
2021-08-04 09:30:43 +0800

    Update thread.py

--- thread.py
+++ thread.py
@@ -55,3 +55,35 @@ for _ in range(10):
 st = time.time()
 gevent.joinall(g)
 print(time.time() - st)
+
+
+
+import threading
+
+class MyThread(threading.Thread):
+    def __init__(self, func, args=()):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)  # 在执行函数的同时，把结果赋值给result,
+        # 然后通过get_result函数获取返回的结果
+
+    def get_result(self):
+        try:
+            return self.result
+        except Exception as e:
+            return None
+        
+result = []
+threads = []
+t = MyThread(func1, args=(var))
+t.start()
+threads.append(t)
+t = MyThread(func2, args=(var))
+t.start()
+threads.append(t)
+for t in threads:
+    t.join()  # 一定执行join,等待子进程执行结束，主进程再往下执行
+    result.append(t.get_result())
